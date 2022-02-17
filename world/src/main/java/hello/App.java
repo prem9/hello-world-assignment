0343a23db10911fa676c536fb188d5969f6fa308
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-02-16 21:06:33 -0800

    Added hello world
    used maven project

---
+++ world/src/main/java/hello/App.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Prem Agarwal
+ * Hello world!
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
